afe56729cb47dc8e2c58f5070117f3e58ac405ff
meghan87 <115980448+meghan87@users.noreply.github.com>
2022-11-26 11:23:44 +0530

    chat start functionality added

--- app.js
+++ app.js
@@ -10,14 +10,21 @@ toggleBtn.addEventListener("click",function(){
 
 
 //chat
+
+//chat btn click
 const chatBtn = document.querySelector(".chat-btn");
 const chatScreen = document.querySelector(".chat__screen");
 chatBtn.addEventListener("click",function(){
     chatScreen.classList.toggle("show-chat__screen")
 })
 
-const chatForm = document.querySelector(".chat-conversation__form");
-chatForm.addEventListener("submit",function(e){
-    e.preventDefault();
-    
+
+//chat start
+const chatStartBtn = document.querySelector("#chat__start__btn")
+const chatMain = document.querySelector(".chat__main")
+chatStartBtn.addEventListener("click",function(){
+    chatMain.classList.add("show-chat__main");
+    chatStartBtn.parentElement.style.display="none";
 })
+
+
